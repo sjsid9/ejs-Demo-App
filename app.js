5d004f6e14dea17f97675facb02c86b5fc8bd2c3
Siddhant <sjsid9@gmail.com>
2020-01-03 13:26:15 +0530

    css through linking and .ejs extension removal and look for folders while serving app
    Signed-off-by: Siddhant <sjsid9@gmail.com>

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-// var express = require("express");
-// var app = express();
+var express = require("express");
+var app = express();
 
-var app = require("express")();
+// var app = require("express")();
+
+app.use(express.static("public"));
+app.set("view egine", "ejs");
 
 app.get("/", function (req, res) {
     // res.send("<h1>Welcome to the home Page<h1>");
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/posts", function (req, res) {
@@ -14,12 +17,12 @@ app.get("/posts", function (req, res) {
         { title: "Ironman", author: "Tony Stark" },
         { title: "Captain America", author: "Steve Rogers" }
     ]
-    res.render("posts.ejs", { posts: posts });
+    res.render("posts", { posts: posts });
 });
 
 app.get("/filw/:thing", function (req, res) {
     var thing = req.params.thing;
-    res.render("love.ejs", { thingVar: thing });
+    res.render("love", { thingVar: thing });
 });
 
 app.listen(3000, function (req, res) {
